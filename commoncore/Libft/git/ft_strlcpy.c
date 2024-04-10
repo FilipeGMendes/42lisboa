e3f916a5cdf8d09495c4c34f8e1773b0024c5630
Filipe Mendes <filipe0505mendes@gmail.com>
2024-04-10 15:59:58 +0100

    Add files via upload

--- commoncore/Libft/git/ft_strlcpy.c
+++ commoncore/Libft/git/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: fguerrei <fguerrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 14:51:13 by fguerrei          #+#    #+#             */
-/*   Updated: 2024/04/10 14:53:18 by fguerrei         ###   ########.fr       */
+/*   Updated: 2024/04/10 15:51:39 by fguerrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 size_t ft_strlcpy(char *dst, const char *src, size_t size)
 {
+	size_t len;
+	size_t i;
 	
+	if (!dst || !src)
+		return (0);
+	
+	len = ft_strlen(src);
+	i = 0;
+	if (len != 0)
+	{
+		while (src[i] != '\0' && i < (len - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (len);
 }
