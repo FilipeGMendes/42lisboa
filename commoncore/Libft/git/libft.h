2931edeafa2004e04abcca33ea9efd87c2d7bad4
Filipe Mendes <filipe0505mendes@gmail.com>
2024-04-14 21:42:02 +0100

    update

--- commoncore/Libft/git/libft.h
+++ commoncore/Libft/git/libft.h
@@ -6,7 +6,7 @@
 /*   By: filipemendes <filipemendes@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 14:19:56 by fguerrei          #+#    #+#             */
-/*   Updated: 2024/04/14 16:16:10 by filipemende      ###   ########.fr       */
+/*   Updated: 2024/04/14 20:28:35 by filipemende      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ int 	ft_strncmp(const char *s1, const char *s2, size_t n);
 char 	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char 	*ft_strdup(const char *s1);
 char 	*ft_strcpy(char *dest, const char *src);
+char 	*ft_substr(char const *s, unsigned int start, size_t len);
 
 void 	*ft_memset(void *s, int c, size_t n);
 void 	ft_bzero(void *s, size_t n);
@@ -42,9 +43,6 @@ void 	*ft_memmove(void *dest, const void *src, size_t n);
 void 	*ft_memchr(const void *s, int c, size_t n);
 int 	ft_memcmp(const void *s1, const void *s2, size_t n);
 void 	*ft_calloc(size_t count, size_t size);
-
-
-
 
 int 	ft_toupper(int c);
 int 	ft_tolower(int c);
