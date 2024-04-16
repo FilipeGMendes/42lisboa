603980bd11d2570bce286d8c84678bcf9a3f8f43
Filipe Mendes <filipe0505mendes@gmail.com>
2024-04-16 17:46:40 +0100

    update

--- commoncore/Libft/git/ft_split.c
+++ commoncore/Libft/git/ft_split.c
@@ -3,32 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fguerrei <fguerrei@student.42.fr>          +#+  +:+       +#+        */
+/*   By: filipemendes <filipemendes@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 11:28:50 by fguerrei          #+#    #+#             */
-/*   Updated: 2024/04/16 13:37:57 by fguerrei         ###   ########.fr       */
+/*   Updated: 2024/04/16 16:30:37 by filipemende      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int ft_countwords(char const *s, char c)
+static int	ft_countwords(char const *s, char c)
 {
-	int i;
-	int trigger;
-	int count;
+	int	i;
+	int	trigger;
+	int	count;
 
 	i = 0;
 	trigger = 0;
 	count = 0;
 	while (s[i])
 	{
-		if(s[i] != c && trigger == 0)
+		if (s[i] != c && trigger == 0)
 		{
 			trigger = 1;
 			count++;
 		}
-		else if(s[i] == c)
+		else if (s[i] == c)
 			trigger = 0;
 		i++;
 	}
@@ -42,15 +42,16 @@ char	**ft_split(char const *s, char c)
 	size_t j;
 
 	i = 0;
-	result = (char **)malloc(sizeof(char *) * (ft_countwords(s, c)+ 1));
+	j = 0;
+	result = (char **)malloc(sizeof(char *) * (ft_countwords(s, c) + 1));
 	if (!result)
 		return (NULL);
-	while(*s)
+	while (*s)
 	{
 		while (*s && *s == c)
 			s++;
-		if(*s)
-		{	
+		if (*s)
+		{
 			if (!ft_strchr(s, c))
 				j = ft_strlen(s);
 			else
