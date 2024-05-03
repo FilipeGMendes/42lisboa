f031ffe4b8f0563e57e1b808f470965113cc1437
Filipe Mendes <filipe0505mendes@gmail.com>
2024-05-03 16:31:32 +0100

    update

--- commoncore/rank01/ft_printf/ft_printf.h
+++ commoncore/rank01/ft_printf/ft_printf.h
@@ -1,38 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fguerrei <fguerrei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/30 10:35:54 by fguerrei          #+#    #+#             */
-/*   Updated: 2024/05/01 11:28:17 by fguerrei         ###   ########.fr       */
+/*   Created: 2024/05/01 12:29:33 by fguerrei          #+#    #+#             */
+/*   Updated: 2024/05/03 16:28:09 by fguerrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-void	ft_putnbr_fd(int n, int fd)
-{
-	if (n == -2147483648)
-	{
-		write(fd, "-2147483648", 11);
-	}
-	else
-	{
-		if (n < 0)
-		{
-			ft_putchar_fd('-', fd);
-			n = -n;
-		}
-		if (n > 9)
-		{
-			ft_putnbr_fd(n / 10, fd);
-			ft_putnbr_fd(n % 10, fd);
-		}
-		if (n < 10)
-			ft_putchar_fd(n + '0', fd);
-	}
-}
-/* this one works like putnbr
-*/
+# include <unistd.h>
+# include <stdarg.h>
+
+int		ft_printf(const char *str, ...);
+int		ft_printchar(char c);
+int		ft_printstr(char *str);
+int		ft_printdigits(int digits, size_t n);
+
+#endif
